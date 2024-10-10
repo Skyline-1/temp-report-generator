82d942f9b0ed353f48b9fcc5b41ac61e5108ce7c
parneetkaur231997@gmail.com <parneetkaur231997@gmail.com>
2024-10-10 17:50:55 +0000

    Edited server

--- server_code/server.py
+++ server_code/server.py
@@ -4,9 +4,15 @@ import pandas as pd
 def save_user_choice(selected_value, author_name, start_date, start_time, end_date, end_time, files, temp_value):
     print("Selected option: ", selected_value)
     print("Author name: ", author_name)
-    start_datetime = pd.to_datetime(start_date + ' ' + start_time)
-    end_datetime = pd.to_datetime(end_date + ' ' + end_time)
+    print("start date: ", start_date)
+    print("start time: ", start_time)
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    start_str = start_date_str + " " + start_time
+    start_datetime = pd.to_datetime(start_str)
     print("Start date time: ", start_datetime)
+    end_date_str = end_date.strftime('%Y-%m-%d')
+    end_str = end_date_str + " " + end_time
+    end_datetime = pd.to_datetime(end_str)
     print("End date time: ", end_datetime)
     print("Temp: ", temp_value)
     
